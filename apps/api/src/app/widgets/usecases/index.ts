3f0a30a5483600e4c4da165e3452d5a7c16d68b1
Biswajeet Das <biswajeetdas18@gmail.com>
2023-01-31 16:03:24 +0530

    chore: created usecase

--- apps/api/src/app/widgets/usecases/index.ts
+++ apps/api/src/app/widgets/usecases/index.ts
@@ -1,3 +1,4 @@
+import { MarkAllMessageAsSeenByFeed } from './mark-all-message-as-seen-by-feed/mark-all-message-as-seen-by-feed.usecase';
 import { GetOrganizationData } from './get-organization-data/get-organization-data.usecase';
 import { MarkMessageAs } from './mark-message-as/mark-message-as.usecase';
 import { GetNotificationsFeed } from './get-notifications-feed/get-notifications-feed.usecase';
@@ -8,6 +9,7 @@ import { MarkAllMessageAsSeen } from './mark-all-message-as-seen/mark-all-messag
 import { GetFeedCount } from './get-feed-count/get-feed-count.usecase';
 
 export const USE_CASES = [
+  MarkAllMessageAsSeenByFeed,
   GetOrganizationData,
   UpdateMessageActions,
   MarkMessageAs,
